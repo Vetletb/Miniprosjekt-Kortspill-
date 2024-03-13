9840f20775af6b60ca002fe15009651a6e80001b
Vetle <vetletb@stud.ntnu.no>
2024-03-13 22:55:11 +0100

    Made dealHand method in DeckOfCards

--- src/main/java/edu/ntnu/idatt2003/DeckOfCards.java
+++ src/main/java/edu/ntnu/idatt2003/DeckOfCards.java
@@ -2,7 +2,7 @@ package edu.ntnu.idatt2003;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Random;
 
 public class DeckOfCards {
 
@@ -15,5 +15,16 @@ public class DeckOfCards {
         deck.add(new PlayingCard(s, f));
       }
     }
+  }
+
+  public List<PlayingCard> dealHand(int n) {
+    Random random = new Random();
+    List<PlayingCard> hand = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      int card = random.nextInt(deck.size());
+      hand.add(deck.get(card));
+      deck.remove(card);
+    }
+    return hand;
   }
 }
